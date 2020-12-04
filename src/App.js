59184f3a739d50000163ddbda842757aad3e60f2
hamza.hashmee <hamza.hashmee@ibstel.com>
2020-12-04 12:40:09 +0500

    about us design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './Components/Home/Home';
+import Aboutus from './Components/AboutUs/AboutUs';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <Home />
+        <Router>
+          <Route path="/" exact component={Home} />
+          <Route path="/aboutus" exact component={Aboutus} />
+        </Router>
       </div>
     );
 
